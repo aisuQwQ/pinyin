704828d2436444c4028f99aa5dcdfe073f806dd5
aisuQwQ <aisuQwQ@ya.ru>
2024-06-17 10:09:50 +0900

    feat: order

--- public/index.ts
+++ public/index.ts
@@ -1,17 +1,21 @@
 /// <reference lib="dom"/>
 let tab:Window|null;
+let desc=false;
 
 //ボタンで発火するかんすう
 async function btn(){
     const input=document.querySelector("input")
     if(input==null) return;
-    const text=input.value;
+    const text=input.value.trim();
     input.value=''
+    if(text==='') return;
     
     const res=await fetch("/send"+"?s="+text)
     const pinyins=await res.json()
     createDom(pinyins, text)
 }
+
+
 
 //音声再生用のかんすう
 const audioContext = new(window.AudioContext || window.webkitAudioContext)();
@@ -59,13 +63,29 @@ async function createDom(pinyins: string[], letters: string){
 
     //コンテナについか
     const containerDOM=document.querySelector('#container');
-    if(containerDOM!=null)
-        containerDOM.insertBefore(rowDOM, containerDOM.firstChild)
+    if(containerDOM==null) return;
+    if(desc)
+        containerDOM.appendChild(rowDOM);
+    else
+        containerDOM.insertBefore(rowDOM, containerDOM.firstChild);
 
     sentenceDOM.addEventListener('click', ()=>{
         if(tab)tab.close()
         tab=window.open('https://www.deepl.com/en/translator#zh/ja/'+letters, 'pinyin')
     });
+}
+
+function changeorder(orderbtn:HTMLElement){
+    //toggleだとロード時もうごいちゃうから
+    if(desc==false){
+        desc=true;
+        orderbtn.classList.add('desc');
+        orderbtn.classList.remove('asc');
+    }else{
+        desc=false;
+        orderbtn.classList.add('asc');
+        orderbtn.classList.remove('desc');
+    }
 }
 
 window.onload=()=>{
@@ -78,4 +98,9 @@ window.onload=()=>{
             if(e.key==="Enter")
                 btn()
         })
+    const orderbtn=document.getElementById('order');
+    if(orderbtn)
+        orderbtn.addEventListener('click', ()=>{
+            changeorder(orderbtn)
+        });
 }
